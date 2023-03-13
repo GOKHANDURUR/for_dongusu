99bb964e94d7d2452c0d277024c8b3a0738bd0d0
GOKHANDURUR <123666133+GOKHANDURUR@users.noreply.github.com>
2023-03-13 19:36:17 +0300

    for döngüsü çalışşması

---
+++ for_donguleri/for_donguleri/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_donguleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ekrandan girilen sayıya kadar olan tek sayıları ekrana yazdırır.
+
+            Console.WriteLine("Bir Sayi Giriniz: ");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                    Console.WriteLine(i);
+            }
+
+            //1 ile 1000 arasındaki tek ve çift sayıların kendi içlerindeki toplamlarını ekrana yazıdırır.
+
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    tekToplam += i;
+                }
+                else
+                    ciftToplam += i;
+            }
+            Console.WriteLine("Tek Toplam: " + tekToplam);
+            Console.WriteLine("Cift Toplam: " + ciftToplam);
+
+            //break, continue
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    break;
+                Console.WriteLine(i);
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                if (i == 4)
+                    continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
